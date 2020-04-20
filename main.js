0334d54176b923c3333f56321a9295de26f5d1ca
Gabriele Mercurio <gabrielemercurio.web@gmail.com>
2020-04-20 16:39:49 +0200

    Aggiornamento

--- main.js
+++ main.js
@@ -1,20 +1,26 @@
 // Chiedere all'utente i chilometri da percorrere
 var chilometri = prompt ('Quanti Km devi percorrere? NB: Inserisci solo dati numerici');
 console.log (chilometri);
+
 // Chiedere all'utente la sua età
 var eta = prompt ('Quanti anni hai? NB: Inserisci solo dati numerici');
 console.log (eta);
+
 // Calcolare il Prezzo in base ai Chilometri da percorrere (0,21€/Km)
 var prezzo = (0.21 * chilometri);
+
 // Applicare sconto del 20% per minorenni
 // Applicare sconto del 40% per over 65enni
 if (eta < 18) {
     console.log (prezzo - ((prezzo * 20) / 100) + '€');
+    // Stampare il risultato finale e mostrarlo all'utente
     document.write('Poiché hai meno di 18 anni, verrà applicato uno sconto del 20%. Totale: ' + (prezzo - ((prezzo * 20) / 100) + '€'));
 } else if (eta >= 65) {
     console.log (prezzo - ((prezzo * 40) / 100) + '€');
+    // Stampare il risultato finale e mostrarlo all'utente
     document.write('Poiché sei un over 65, verrà applicato uno sconto del 40%. Totale: ' + (prezzo - ((prezzo * 40) / 100) + '€'));
 } else {
-    console.log ('Totale: ' + (prezzo + '€'));
+    console.log ((prezzo + '€'));
+    // Stampare il risultato finale e mostrarlo all'utente
+    document.write('Totale: ' + (prezzo + '€'));
 }
-// Stampare il risultato finale e mostrarlo all'utente
